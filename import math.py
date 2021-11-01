263a9217142b395441e86bd876e54f1259158634
Maxim687 <90177556+Maxim687@users.noreply.github.com>
2021-11-01 16:09:49 +0200

    Update import math.py

--- import math.py
+++ import math.py
@@ -3,14 +3,19 @@ x =int(input("x:"))
 a =int(input("a:"))
 b =int(input("b:"))
 e = a * b
-z = e ** x + math.sin(x) - math.cos(x) * 2
+z = (int(e)** x + (math.sin(x)) - (math.cos(2 * x)))
 print (str(z))
 n = int(input("Введіть ціле число:"))
-print("Результат:", end = " ")
+print("Прості дільники числа:", end = " ")
 for i in range(n - 1,1,-1):
-    if(n % i ==0):
-        print(i,end = " ")
+    is_simple = 0
+    if (n % i ==0):
+        for j in range(i -1,1,-1):
+            if (i % j == 0):
+                is_simple = is_simple + 1
+        if (is_simple ==0):
+            print(i, end = " ")       
 N = [-7,-5,-3,-2,1,3,5,7]
 print("Найменше число N:" + str(min(N)))
 F = [-7,-5,-3,-2]
-print(str(sum (F)))    
+print("Сума від'ємних чисел:" + str(sum (F)))    
